65637e2042269cb806217e181f3039867c709ee1
khris <khris56@hotmail.com>
2019-07-30 12:37:40 -0500

    repeater implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      form: ""
+    }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" value={this.state.form} onChange={this.updateForm.bind(this)} />
+        <p className="repeater">{this.state.form}</p>
       </div>
     );
+  }
+
+  updateForm(event) {
+    this.setState({
+      form: event.target.value
+    })
   }
 }
 
